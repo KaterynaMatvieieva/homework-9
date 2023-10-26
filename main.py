dae9edda4692d3f89b48fc1bfe85fadc1ed0a5db
Kateryna Matvieieva <kateryna_matvieieva@epam.com>
2023-10-26 23:15:44 +0300

    Added tasks

---
+++ main.py
@@ -0,0 +1,35 @@
+#Даний текстовий файл. Необхідно створити новий файл, який потрібно переписати з першого файлу всі слова,
+#що складаються не менше ніж з семи літер.
+
+# OLD_FILE="old_file.txt"
+# NEW_FILE="new_file.txt"
+# result_string = ""
+#
+# with open(OLD_FILE, "w") as file:
+#     file.write(input("Enter some text to be analyzed: "))
+#
+# with open(OLD_FILE, "r") as file:
+#     content = file.read()
+#     content = content.replace(".","")
+#     content = content.replace(",", "")
+#     content = content.replace("!", "")
+#     content = content.replace("?", "")
+#     for word in content.split():
+#         if len(word) >= 7:
+#             result_string += word + ", "
+#
+# with open(NEW_FILE, "w") as file:
+#      file.write(result_string)
+
+#Даний текстовий файл. Підрахувати кількість слів у ньому.
+
+# OLD_FILE="old_file.txt"
+#
+# with open(OLD_FILE, "r") as file:
+#     content = file.read()
+#     content = content.replace(".","")
+#     content = content.replace(",", "")
+#     content = content.replace("!", "")
+#     content = content.replace("?", "")
+#     words = content.split()
+#     print(len(words))
